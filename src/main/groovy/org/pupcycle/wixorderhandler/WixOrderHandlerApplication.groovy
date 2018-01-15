d0dd9dfbebc7c9c4ad00d79fbe8df0cfba608f52
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-01-14 22:41:48 -0600

    Initial Commit

---
+++ src/main/groovy/org/pupcycle/wixorderhandler/WixOrderHandlerApplication.groovy
@@ -0,0 +1,12 @@
+package org.pupcycle.wixorderhandler
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WixOrderHandlerApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run WixOrderHandlerApplication, args
+	}
+}
