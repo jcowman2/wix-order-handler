b62f3311638f9aa5c26d11a1c76680e29661b5f6
Joe Cowman <joe.cowman@huskers.unl.edu>
2018-02-22 07:11:34 -0600

    Implemented basic logging with Slf4j

--- src/main/groovy/org/pupcycle/wixorderhandler/OrderHandlerRunner.groovy
+++ src/main/groovy/org/pupcycle/wixorderhandler/OrderHandlerRunner.groovy
@@ -3,6 +3,8 @@ package org.pupcycle.wixorderhandler
 import com.google.api.services.gmail.Gmail
 import com.google.api.services.gmail.model.Label
 import com.google.api.services.gmail.model.ListLabelsResponse
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
@@ -10,11 +12,15 @@ import org.springframework.stereotype.Component
 @Component
 class OrderHandlerRunner implements CommandLineRunner {
 
+    private static final Logger LOG = LoggerFactory.getLogger(OrderHandlerRunner.class)
+
     @Autowired
     Gmail gmailService
 
     @Override
     void run(String... args) throws Exception {
+        LOG.info("OrderHandlerRunner started successfully.")
+
         // Print the labels in the user's account.
         String user = "me"
         ListLabelsResponse listResponse =
